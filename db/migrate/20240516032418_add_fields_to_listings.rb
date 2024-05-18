335912baece0ead3b5702b17a782adb31a62b61a
Luciano Torrano <luciano.torrano@zenithtek.io>
2024-05-17 23:51:44 -0300

    validation in db fields added

--- db/migrate/20240516032418_add_fields_to_listings.rb
+++ db/migrate/20240516032418_add_fields_to_listings.rb
@@ -1,10 +1,10 @@
 class AddFieldsToListings < ActiveRecord::Migration[7.1]
   def change
-    add_column :listings, :image_url, :string
-    add_column :listings, :is_new, :boolean
-    add_column :listings, :is_favorite, :boolean
-    add_column :listings, :price, :string
-    add_column :listings, :specifications, :string
-    add_column :listings, :real_estate_name, :string
+    add_column :listings, :image_url, :string unless column_exists?(:listings, :image_url)
+    add_column :listings, :is_new, :boolean unless column_exists?(:listings, :is_new)
+    add_column :listings, :is_favorite, :boolean unless column_exists?(:listings, :is_favorite)
+    add_column :listings, :price, :string unless column_exists?(:listings, :price)
+    add_column :listings, :specifications, :string unless column_exists?(:listings, :specifications)
+    add_column :listings, :real_estate_name, :string unless column_exists?(:listings, :real_estate_name)
   end
 end
